c7f67936bf1676fab535ef388a49e4b36955ac52
mathematiCode <160360849+mathematiCode@users.noreply.github.com>
2024-10-12 12:52:47 -0500

    Grid styles implemented and 6 guess limit

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,11 +1,34 @@
 import React from "react";
 
 function PreviousGuesses({ guessList }) {
+  let temporaryGuessList = [...guessList];
+  for (let i = 0; i < 6; i++) {
+    if (!temporaryGuessList[i]) {
+      temporaryGuessList[i] = "     ";
+    }
+    if (temporaryGuessList.length > 6) {
+      temporaryGuessList = temporaryGuessList.slice(0, 6);
+    }
+  }
   return (
     <ol className="guess-results">
-      {guessList.map((guess, index) => (
+      {temporaryGuessList.map((guess, index) => (
         <li key={`guess #${index}: ${guess}`} className="guess">
-          {guess}
+          <span key={1} className="cell">
+            {guess[0]}
+          </span>
+          <span key={2} className="cell">
+            {guess[1]}
+          </span>
+          <span key={3} className="cell">
+            {guess[2]}
+          </span>
+          <span key={4} className="cell">
+            {guess[3]}
+          </span>
+          <span key={5} className="cell">
+            {guess[4]}
+          </span>
         </li>
       ))}
     </ol>
