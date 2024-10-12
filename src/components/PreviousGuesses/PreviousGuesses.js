f66409fcef1f6dc933bd7a8b2352140526d02f3c
mathematiCode <160360849+mathematiCode@users.noreply.github.com>
2024-10-12 11:44:25 -0500

    added a key to each guess

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function PreviousGuesses({ guessList }) {
   return (
-    <ol class="guess-results">
-      {guessList.map((guess) => (
-        <li class="guess">{guess}</li>
+    <ol className="guess-results">
+      {guessList.map((guess, index) => (
+        <li key={`guess #${index}: ${guess}`} className="guess">
+          {guess}
+        </li>
       ))}
     </ol>
   );
